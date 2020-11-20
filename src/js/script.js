ff2ca1d73c264ed615cb890a7bf0574b0638f8a5
Nancy Cruz <nancy.pcrz@gmail.com>
2020-11-20 03:25:09 -0600

    fase 1 el futuro florece funcional

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,21 @@ $(document).ready(function(){
       });
     } // End if
   });
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("activation");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
   
   $(window).scroll(function() {
     $(".slideanim").each(function(){
